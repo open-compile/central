4012afc2d2a67201151b75e4dc41605760f3008f
jason.lu <jason-lu.mbp>
2020-06-26 21:52:46 +0800

    Add missing file

---
+++ opt/opt_main.h
@@ -0,0 +1,11 @@
+//
+// Created by xc5 on 2020/6/20.
+//
+
+#ifndef OCC_OPT_MAIN_H
+#define OCC_OPT_MAIN_H
+
+// Emitting data section
+INT32 Emit_section_data(FILE *out, FILE_MANAGER *man);
+
+#endif //OCC_OPT_MAIN_H
