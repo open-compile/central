985f768ab310761d81429d5e3a7d9f150326cbb1
徐铭 <786106192@qq.com>
2020-07-22 17:53:11 +0800

    test

--- fe/fe_main.cxx
+++ fe/fe_main.cxx
@@ -83,6 +83,10 @@ void Irgen_visit(NBlock *block) {
   }
 }
 
+/**
+ * 处理某个function
+ * @param func
+ */
 void visitFunction(const shared_ptr<NFunctionDeclaration> &func) {
   STR_IDX func_name = File()->Save_string(func->id->name.c_str());
   TY_IDX ty_i4 = MTYPE_to_ty(MTYPE_I4);
